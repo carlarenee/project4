4a8b302ae739bd87c930b159734619e035eacaf9
carla stockalper <cstockalper@gmail.com>
2016-12-14 14:54:51 -0500

    add dummy data to seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,11 @@
 INSERT INTO users (username, password) VALUES ('carla', '123');
 
 INSERT INTO watson (saved_on, anger_score, disgust_score, fear_score, joy_score, sadness_score, username)
-VALUES ('2016-12-12', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'carla'),
+VALUES ('2016-12-15', '0.2145657', '0.787528', '0.99961', '0.873854', '0.9082709', 'carla'),
+       ('2016-12-14', '0.142021', '0.209965', '0.430666', '0.298084', '0.268845', 'carla'),
+       ('2016-12-13', '0.437327', '0.456828', '0.98761', '0.893854', '0.482709', 'carla'),
+       ('2016-12-12', '0.144327', '0.174828', '0.44961', '0.143854', '0.482709', 'carla'),
+       ('2016-12-12', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'carla'),
        ('2016-12-11', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'carla'),
        ('2016-12-10', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'carla'),
        ('2016-12-09', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'carla'),
@@ -13,13 +17,30 @@ VALUES ('2016-12-12', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709',
 INSERT INTO users (username, password) VALUES ('jo', '123');
 
 INSERT INTO watson (saved_on, anger_score, disgust_score, fear_score, joy_score, sadness_score, username)
-VALUES ('2016-12-12', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'jo'),
+VALUES ('2016-12-15', '0.3245657', '0.567528', '0.32961', '0.233854', '0.1282709', 'jo'),
+       ('2016-12-14', '0.142021', '0.209965', '0.430666', '0.298084', '0.268845', 'jo'),
+       ('2016-12-13', '0.437327', '0.456828', '0.98761', '0.893854', '0.482709', 'jo'),
+       ('2016-12-12', '0.144327', '0.174828', '0.44961', '0.143854', '0.482709', 'jo'),
        ('2016-12-11', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'jo'),
        ('2016-12-10', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'jo'),
        ('2016-12-09', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'jo'),
        ('2016-12-08', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'jo'),
        ('2016-12-07', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'jo'),
        ('2016-12-06', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'jo');
+
+INSERT INTO users (username, password) VALUES ('bo', '123');
+
+INSERT INTO watson (saved_on, anger_score, disgust_score, fear_score, joy_score, sadness_score, username)
+VALUES ('2016-12-15', '0.324327', '0.567828', '0.11961', '0.643854', '0.8982709', 'bo'),
+       ('2016-12-14', '0.342021', '0.109965', '0.080666', '0.098084', '0.268845', 'bo'),
+       ('2016-12-13', '0.777327', '0.674828', '0.98761', '0.893854', '0.482709', 'bo'),
+       ('2016-12-12', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'bo'),
+       ('2016-12-11', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'bo'),
+       ('2016-12-10', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'bo'),
+       ('2016-12-09', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'bo'),
+       ('2016-12-08', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'bo'),
+       ('2016-12-07', '0.144327', '0.174828', '0.11961', '0.143854', '0.482709', 'bo'),
+       ('2016-12-06', '0.542021', '0.109965', '0.080666', '0.098084', '0.268845', 'bo');
 
 
 
@@ -50,14 +71,20 @@ VALUES ('Speak when you''re angry, and you''ll make the best speech you''ll ever
        ('If you know the enemy and know yourself you need not fear the results of a hundred battles.', 'Sun Tzu', 'fear'),
        ('Being happy never goes out of style.', 'Lilly Pulitzer', 'joy'),
        ('Don''t waste a minute not being happy. If one window closes, run to the next window or break down a door.', 'Brooke Shields', 'joy'),
-       ('Don''t be sad', 'Me', 'sadness');
-
-
-
-
-
-
-
-
-
+       ('When you rise in the morning, give thanks for the light, for your life, for your strength. Give thanks for your food and for the joy of living. If you see no reason to give thanks, the fault lies in yourself', 'Tecumseh', 'joy'),
+       ('Joy in looking and comprehending is nature''s most beautiful gift.', 'Albert Einstein', 'joy'),
+       ('We are shaped by our thoughts; we become what we think. When the mind is pure, joy follows like a shadow that never leaves.', 'Buddha', 'joy'),
+       ('Happiness lies in the joy of achievement and the thrill of creative effort.', 'Franklin D. Roosevelt', 'joy'),
+       ('It is the supreme art of the teacher to awaken joy in creative expression and knowledge.', 'Albert Einstein', 'joy'),
+       ('Gratitude can transform common days into thanksgivings, turn routine jobs into joy, and change ordinary opportunities into blessings.', 'William Arthur Ward', 'joy'),
+       ('Let my soul smile through my heart and my heart smile through my eyes, that I may scatter rich smiles in sad hearts. ', 'Paramahansa Yogananda', 'sadness'),
+       ('Tears come from the heart and not from the brain.', 'Leonardo DaVinci', 'sadness'),
+       ('The good times of today, are the sad thoughts of tomorrow.', 'Bob Marley', 'sadness'),
+       ('It''s sad to know I''m done. But looking back, I''ve got a lot of great memories.', 'Bonnie Blair', 'sadness'),
+       ('First, accept sadness. Realize that without losing, winning isn''t so great.', 'Alyssa Milano', 'sadness'),
+       ('The word ''happy'' would lose its meaning if it were not balanced by sadness.', 'Carl Jung', 'sadness')
+       ('Your pain is the breaking of the shell that encloses your understanding.', 'Khalil Gibran', 'sadness'),
+       ('Affliction comes to us, not to make us sad but sober; not to make us sorry but wise.', 'H. G. Wells', 'sadness'),
+       ('The rose and the thorn, and sorrow and gladness are linked together.', 'Saadi', 'sadness'),
+       ('Sadness is but a wall between two gardens.', 'Khalil Gibran', 'sadness');
 
